6affe072c549884991e779cff832a33ae27108af
Matheus Fay <mfaysoares@gmail.com>
2020-09-02 14:31:13 -0300

    Add files via upload

---
+++ pep8.py
@@ -0,0 +1,21 @@
+"""
+PEP8 - Python Enhancement Proposal
+Propostas de melhoria para a linguagem Python
+
+Zem of Python
+import this
+
+A ideia da PEP8 é que possamos escrever códigos Python de forma Pythônica.
+
+[1] - Utilize Camel Cases para nomes de classe;
+[2] - Utilize nomes em minusculo, separados por underline para funções ou variáveis;
+[3] - Utilize 4 espaços para identação! Não utilizar TAB
+[4] - Linhas em branco
+    *Separa funções e definições de classe com duas linhas em branco;
+    *Métodos dentro de uma classe devem ser separados com uma linha em branco
+[5] - Imports devem sempre ser feitos em linhas separadas;
+[6] - Espaços em expressões e instruções;
+[7] - Termine sempre uma instrução com uma nova linha (Sempre deixar uma linha em branco no final)
+"""
+
+import this
